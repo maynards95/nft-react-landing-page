320f7cec5b51234c55b98b01432f7c4daec39e31
Ezra Maynard <maynard.c.ezra@gmail.com>
2024-02-01 07:45:26 -0600

    Adds our first NFT card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SponsorItemList from '@/components/SponsorItemList'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Nft19941 from '@/assets/nft-19941.png'
+import NftCard from '@/components/NftCard'
 
 function App() {
   return (
@@ -11,6 +12,13 @@ function App() {
       <div className="page-container">
         <Hero heroImage={Nft19941} />
         <SponsorItemList />
+        <NftCard
+          image={Nft19941}
+          name="Abmord"
+          price="2.99"
+          daysLeft={10}
+          number={23323}
+        />
       </div>
     </div>
   )
---
+++ src/components/NftCard/index.jsx
@@ -0,0 +1,28 @@
+import './style.css'
+import EthereumLogo from '@/assets/ethereum-logo.png'
+
+function NftCard(props) {
+  const { image, number, name, price, daysLeft } = props
+  return (
+    <div className="nft-card">
+      <img className="nft-card_img" src={image} alt={`nft-card-${name}`} />
+      <div className="nft-card_details">
+        <span className="nft-card_details-series">Gloop series</span>
+        <span className="nft-card_details-top-bid">Top bid</span>
+        <span className="nft-card_details-name">{name}</span>
+        <span className="nft-card_details-price">
+          <img
+            className="nft-card_details-ethereum-logo"
+            src={EthereumLogo}
+            alt="ethereum-logo"
+          />{' '}
+          {price}
+        </span>
+        <span className="nft-card_details-number">#{number}</span>
+        <span className="nft-card_details-days-left">{daysLeft} days left</span>
+      </div>
+    </div>
+  )
+}
+
+export default NftCard
