321a316bd6a1c6e24f1c0c369bed51374cabf385
Ezra Maynard <maynard.c.ezra@gmail.com>
2024-02-03 11:24:19 -0600

    Adds style to our NFT card list(Completes NFT Card List Component)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Nft19941 from '@/assets/nft-19941.png'
 import NftCard from '@/components/NftCard'
+import NtfCardList from './components/NftCardList'
 
 function App() {
   return (
@@ -12,13 +13,7 @@ function App() {
       <div className="page-container">
         <Hero heroImage={Nft19941} />
         <SponsorItemList />
-        <NftCard
-          image={Nft19941}
-          name="Abmord"
-          price="2.99"
-          daysLeft={10}
-          number={23323}
-        />
+        <NtfCardList />
       </div>
     </div>
   )
---
+++ src/components/NftCardList/index.jsx
@@ -0,0 +1,57 @@
+import './style.css'
+import NftCard from '@/components/NftCard'
+import Nft11381 from '@/assets/nft-11381.png'
+import Nft23232 from '@/assets/nft-23232.png'
+import Nft33333 from '@/assets/nft-33333.png'
+import Nft43234 from '@/assets/nft-43234.png'
+
+const nfts = [
+  {
+    image: Nft11381,
+    name: 'Ezmod',
+    price: 1.99,
+    daysLeft: 15,
+    number: 11381,
+  },
+  {
+    image: Nft23232,
+    name: 'Azmod',
+    price: 1.23,
+    daysLeft: 20,
+    number: 23232,
+  },
+  {
+    image: Nft33333,
+    name: 'Ramod',
+    price: 1.78,
+    daysLeft: 19,
+    number: 33333,
+  },
+  {
+    image: Nft43234,
+    name: 'Ecm',
+    price: 1.56,
+    daysLeft: 25,
+    number: 43234,
+  },
+]
+
+function NftCardList() {
+  return (
+    <div className="nft-card-list">
+      {nfts.map((nft) => {
+        return (
+          <NftCard
+            image={nft.image}
+            name={nft.name}
+            price={nft.price}
+            daysLeft={nft.daysLeft}
+            number={nft.number}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default NftCardList
